67104012798e31765bf973708125b2e05ef5cf07
Mihael Kardash <mixan193@gmail.com>
2023-06-25 14:00:01 +0300

    Добавьте файлы проекта.

---
+++ 2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Кардаш Михаил Андреевич";
+            int age = 29;
+            string email = "mixan193@gmail.com";
+            double mathScores = 5;
+            double phisicsScores = 4;
+            double programmingScores = 3;
+            Console.WriteLine($"Студент {fullName} возрастом {age} лет, \nemail: {email} \nполучил следующие отметки:\nМатематика: {mathScores}\nФизика: {phisicsScores}\nПрограммирование: {programmingScores}");
+            Console.ReadKey();
+        }
+    }
+}
